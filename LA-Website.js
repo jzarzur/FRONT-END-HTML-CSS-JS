95522281df14860e2d2a9775dccc8a000f741e57
Jady Z <53356105+jzarzur@users.noreply.github.com>
2020-02-12 22:19:06 -0300

    Create LA-Website.js

---
+++ LA-Website.js
@@ -0,0 +1,24 @@
+// Used to filter data/ Usado filtrar e relacionar dados.
+
+import wixData from 'wix-data';
+
+$w.onReady (function () {
+});
+
+export function input1_keyPress(event) {
+let searchValue = $w("#input1").value;
+$w("#dataset1").setFilter(wixData.filter().contains('title', searchValue).
+or(wixData.filter().contains('tag', searchValue)).
+or(wixData.filter().contains('cliente', searchValue)));
+}
+
+export function dropdown2_change(event) {
+let dropdownValue = $w("#dropdown2").value;
+$w("#dataset1").setFilter(wixData.filter().contains('cliente', dropdownValue));
+$w("#dropdown2").selectedIndex = 0;
+}
+
+export function button1_click(event) {
+let clienteValue = $w("#dropdown2").value;
+$w("#dataset1").setFilter(wixData.filter());
+}
